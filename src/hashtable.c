4e7db5d6b93d53cfb838bce62fe5236257b6bc1b
Shinwoo Kim <shinwookim@pitt.edu>
2023-07-04 22:58:22 -0400

    Add support for collision resolution

--- src/hashtable.c
+++ src/hashtable.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include "hashtable.h"
 
+static ht_item HT_REMOVED_ITEM = {NULL, NULL};
+
 // Tester
 int main(void)
 {
@@ -40,7 +42,7 @@ void remove_table(ht_hashtable *table)
     for (int i = 0; i < table->size; i++)
     {
         ht_item *item = table->items[i];
-        if (!item)
+        if (item)
             remove_item(item);
     }
     free(table->items);
@@ -62,4 +64,82 @@ int hash(const char *key, const int prime, const int m)
         hash = hash % m;
     }
     return (int)hash;
+}
+
+/* Collision handling using open addressing & double hash */
+// i for hash attempt
+static int get_hash(const char *key, const int m, const int i)
+{
+    return (hash(key, HT_PRIME_1, m) + i * (hash(key, HT_PRIME_2, m) + 1)) % m;
+}
+
+// Hashtable API
+
+// hash_table.h
+void insert_item(ht_hashtable *table, const char *key, const char *value)
+{
+    ht_item *item = new_item(key, value);
+    int index = get_hash(item->key, table->size, 0);
+    ht_item *current_item = table->items[index];
+    int iter = 1;
+    while (current_item && current_item != &HT_REMOVED_ITEM)
+    {
+        if (strcmp(current_item->key, key) == 0)
+        {
+            remove_item(current_item);
+            table->items[index] = item;
+            return;
+        }
+        index = get_hash(item->key, table->size, iter);
+        current_item = table->items[index];
+        iter++;
+    }
+    table->items[index] = item;
+    table->count++;
+}
+
+char *search(ht_hashtable *table, const char *key)
+{
+    int index = get_hash(key, table->size, 0);
+    ht_item *item = table->items[index];
+    int iter = 1;
+    while (item)
+    {
+        if (item != &HT_REMOVED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+                return item->value;
+            else
+            {
+                index = get_hash(key, table->size, iter);
+                item = table->items[index];
+                iter++;
+            }
+        }
+    }
+    return NULL;
+}
+
+// Deleting causes issue in open addressing, breaks collision chain,
+// instead mark as removed (using sntinel value)
+void remove_item(ht_hashtable *table, const char *key)
+{
+    int index = get_hash(key, table->size, 0);
+    ht_item *item = table->items[index];
+    int iter = 1;
+    while (item)
+    {
+        if (item != &HT_REMOVED_ITEM)
+        {
+            if (strcmp(item->key, key) == 0)
+            {
+                remove_item(item);
+                table->items[index] = &HT_REMOVED_ITEM;
+            }
+            index = get_hash(key, table->size, iter);
+            item = table->items[index];
+            iter++;
+        }
+    }
+    table->count--;
 }
